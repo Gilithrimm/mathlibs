f81f663dd02114d8df4ca17e4ead00bdc3f067b1
Gilithrimm <jaspidan@wp.pl>
2022-10-02 15:49:51 +0200

    tests need rewriting removing useless code that I don't use anyway (takes too much time to execute)

--- src/main/java/com/safenar/Main.java
+++ src/main/java/com/safenar/Main.java
@@ -1,12 +1,7 @@
 package com.safenar;
 
-import com.safenar.math.*;
-import com.safenar.util.Code;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import static com.safenar.math.MyMath.*;
 
 public class Main {
     public static <T>String toString(T[] arr){
@@ -23,35 +18,8 @@ public class Main {
         return format.format(date);
     }
 
-    public static void measureTime(Code code) throws MathException {
-        long start=System.currentTimeMillis();
-        System.out.println("Started at "+formatTime(start));
-        code.execute();
-        System.out.println("ended at "+formatTime(System.currentTimeMillis()));
-        System.out.println("Took "+(System.currentTimeMillis()-start)+"ms to finish");
-    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws MathException {
-        Code c1=()->{
-            Generator fib= Generators.FIBONACCI;
-            Generator plusOne=x->x+1;
-            Generator lucas=new FibonacciGenerator(1,3);
-            System.out.println("fib High = " + divAvgHigh(fib));
-            System.out.println("fib Low = " + divAvgLow(fib));
-            System.out.println("plusOne High = " + divAvgHigh(plusOne));
-            System.out.println("plusOne Low = " + divAvgLow(plusOne));
-            System.out.println("lucas High = " + divAvgHigh(lucas));
-            System.out.println("lucas Low = " + divAvgLow(lucas));
-            //this goes for 6.5h wtf
-        };
-        Code c2=()-> System.out.println(Generators.FIBONACCI.generate(48));
-        //41=>1s
-        //45=>6.5s
-        //47=>16.7s
-        //48=>25.6s
-
-
-        measureTime(c2);
     }
 
 }
--- src/main/java/com/safenar/util/Code.java
+++
@@ -1,8 +0,0 @@
-package com.safenar.util;
-
-import com.safenar.math.MathException;
-
-@FunctionalInterface
-public interface Code {
-    void execute() throws MathException;
-}
