ee5be359fb3c221cbaf277607819bbd9894bcf71
Gilithrimm <jaspidan@wp.pl>
2022-12-31 16:56:47 +0100

    being an idiot does not help

--- javadoc.sh
+++ javadoc.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 rm -r docs/
-# shellcheck disable=SC2035
 javadoc -protected -splitindex -author -version -d docs/ -tag apiNote:a:"API Note:" -tag implSpec:a:"Implementation Requirements:" -tag implNote:a:"Implementation Note:" -sourcepath src/main/java -subpackages com.safenar
